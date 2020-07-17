326f862cb0b48b2435abf0bedcae0e496a67038d
sakamoto so <sakamoto@farend.jp>
2020-07-17 13:54:19 +0900

    Add test/unit/mailer_test.rb skip.test

--- circleci_files/skip_test.rb
+++ circleci_files/skip_test.rb
@@ -1,4 +1,4 @@
-TOKYO_SKIP_TESTS = {"unit/mailer_test.rb" => %w(test_reminder_should_include_issues_assigned_to_groups test_reminders_for_users test_reminders test_reminders_should_sort_issues_by_due_date test_reminders_language_auto),"functional/issues_controller_test.rb" => %w(test_index_grouped_by_created_on_if_time_zone_is_nil)}
+TOKYO_SKIP_TESTS = {"test/unit/mailer_test.rb" => %w(test_reminder_should_include_issues_assigned_to_groups test_reminders_for_users test_reminders test_reminders_should_sort_issues_by_due_date test_reminders_language_auto),"functional/issues_controller_test.rb" => %w(test_index_grouped_by_created_on_if_time_zone_is_nil)}
 SAMOA_SKIP_TESTS = {"unit/mailer_test.rb" => %w(test_reminder_should_include_issues_assigned_to_groups test_reminders_for_users test_reminders test_reminders_should_sort_issues_by_due_date test_reminders_language_auto)}
 TEST_DIRECTORY_PATH = ARGV[1] || "test" 
 Object.const_get(ARGV[0].upcase).each do |path, test_names|
