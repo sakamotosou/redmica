846401efb427afeb5d144023238d2aaac1844fb6
sakamoto so <sakamoto@farend.jp>
2020-07-17 15:23:18 +0900

    ~/test -

--- circleci_files/change_time_zone.sh
+++ circleci_files/change_time_zone.sh
@@ -4,5 +4,8 @@ if [ -n "$VALUE" ] ; then
     echo "config.time_zone = '${VALUE}'" > config/additional_environment.rb
     bundle exec rails runner "p Time.current" -e test
     bundle exec rake test
+    bundle exec rake redmine:plugins:test NAME=redmine_issue_templates RAILS_ENV=test
+    sed -i "/Options.new/s/$/(args: [\'--headless\', \'--no-sandbox\', \'--disable-gpu\', \'--window-size=1280,800\'])/g" ../../plugins/redmine_issue_templates/spec/rails_helper.rb
+    DRIVER=headless RAILS_ENV=test bundle exec rspec -I ../../plugins/redmine_issue_templates/spec
 fi
 
